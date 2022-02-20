70e3dc4c81c263b3b68bdcd8c216015d4973b515
edde746 <86283021+edde746@users.noreply.github.com>
2022-02-20 09:10:26 +0100

    Sockets & Tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function LiveChat({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default LiveChat
